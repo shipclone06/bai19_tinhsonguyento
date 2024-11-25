a70b4b8819694978286ec71139843a0462ea59b9
shipclone06 <anhkhoa030900@gmail.com>
2024-11-25 14:24:03 +0700

    Update bai19_tinhsonguyento.cpp
    update lan thu N

--- bai19_tinhsonguyento/bai19_tinhsonguyento.cpp
+++ bai19_tinhsonguyento/bai19_tinhsonguyento.cpp
@@ -1,25 +1,31 @@
-// Bai_19-Insonguyen.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 using namespace std;
+bool(kiemtraSNT(int s)) {
+	int dem = 0;
+	for (int i = 1; i <= s; i++)
+	{
+		if (s % i == 0)
+		{
+			dem++;
+		}
+	}
+	if (dem == 2)
+	{
+		return true;
+	}
+	else {
+		return false;
+	}
+}
 int main() {
 	int n;
 	int dem = 0;
 	cout << "nhap so:\n"; cin >> n;
-	for (int y = 1; y <= n; y++) {
-		dem = 0;
-		for (int i = 1; i <= y; i++)
-		{
-			if (y % i == 0) {
-				dem++;
-
-			}
-		}
-		if (dem == 2) {
-			cout << y << " ";
-		}
-	}/*
+	for (int y = 1; y <= n; y++)
+	{
+		if (kiemtraSNT(y) == true) cout << y << " ";
+	}
+	/*
 	for (int i = 1; i <= n; i++)
 	{
 		if (n % i == 0) {
